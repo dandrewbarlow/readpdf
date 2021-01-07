f7e1a681644558bad1861a13cfc515c380f9f2e7
Andrew Barlow <dandrewbarlow@gmail.com>
2021-01-06 18:46:11 -0600

    some formatting and structural changes

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # readpdf by [Andrew Barlow](https://github.com/dandrewbarlow)
-Narrate pdf (and epub) files from the command line
+Narrate pdf (and other) files from the command line
 
 ### Description
 A simple script to take a pdf and narrate it. 
 
-This was written on and for my OSX computer, using OSX's native `say` command as a text to speech device. It also uses ghostscript with OSX syntax. All of which is to say it probably *can* work on other OSes, but it might require some tinkering.
+This was written on and for my OSX computer, using OSX's native `say` command as a text to speech device. It also uses ghostscript with OSX syntax to convert a pdf to text. As of now it also uses pandoc to convert epub files, and will try to implicitly convert any other file types also with pandoc. All of which is to say it probably *can* work on other OSes, but it might require some tinkering.
 
 Fair warning that this is not *professional* grade software, just a script I made for fun. It still has problems, and comes with no warranty or guarantee of any kind.
 ### Requirements
-* Ghostscript - available from [homebrew](https://brew.sh/): `$ brew install ghostscript`
-* pandoc - allows conversion from `epub` format as well
-* ffmpeg - available for download from [here](http://ffmpeg.org/download.html) or again via homebrew: `brew install ffmpeg`. Default audio output for `say` is .aiff, and I for one would like a nice mp3 file
+* Ghostscript - available from [homebrew](https://brew.sh/): `$ brew install ghostscript`. Used to convert pdf's to text.
+* pandoc - available from homebrew: `$ brew install pandoc`. Allows conversion from `epub` format as well as many other formats.
+* ffmpeg - available for download from [here](http://ffmpeg.org/download.html) or again via homebrew: `brew install ffmpeg`. Default audio output for `say` is `.aiff`, and I for one would like a nice `mp3` file. ffmpeg is a notoriously cumbersome program to use, so I didn't allow much customization for conversion options. If you want a different file type, that's something you'll have to manually change.
 * say - included in OSX, replacement needed for other OS's
 
 ### Usage
@@ -18,14 +18,14 @@ Fair warning that this is not *professional* grade software, just a script I mad
 
 - i (argument) input - specify input file
 
+- v (argument) voice - specify voice to use. Defaults to Tom. No real error checking here, just passing it on to `say` so check what's available.
+
 - o (flag) output - if provided, outputs to input.aiff
 
 - c (flag) convert - convert to mp3 using ffmpeg
 
-- v (argument) voice - specify voice to use. Defaults to Tom. No real error checking here, just passing it on to `say` so check what's available.
-
 ### Installation
-Seeing as this is more of a small script than a program, I'm not giving much hard advice on how to install. I like keeping a `~/.scripts/` directory and bash aliases for my scripts, but if you're interested in this, you can probably make an educated decision on this.
+Seeing as this is more of a small script than a program, I'm not giving much hard advice on how to install. I like keeping a `~/.scripts/` directory and bash aliases for my scripts, but if you're interested in this script, you can probably make an educated decision on this.
 
 ### Status
 Ostensibly working. I'd like to remove the need for the temporary text file I create in the script, but it was a good opportunity to learn about the mktemp command (here I was making temp files manually, in place like a clown). It can be pretty dang slow with large pdf's and I've encountered a few errors that I don't know how to fix (yet). I'd like to get a more general purpose version working, but it works on my computer, and I was making it for me. If anyone uses this and wants me to make changes, feel free to ask or take a swing at it yourself.
--- readpdf.sh
+++ readpdf.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-# readpdf - narrate a pdf file with native osx tts utility
+# readpdf - narrate a file with native osx tts utility
 # written by Andrew Barlow
 # https://github.com/dandrewbarlow
 # https://a-barlow.com
 
 
-# POSIX var that can cause trouble if getopts is used multiple times in a row without resetting
+# POSIX var that can apparently cause trouble if getopts is used multiple times in a row without resetting
 OPTIND=1
 
 # initialize empty file vars
@@ -30,16 +30,17 @@ cleanup() {
 trap cleanup EXIT
 trap cleanup SIGINT
 
+# if programs in the script aren't installed, don't run the script
 requirements() {
-	if [ ! -x "$(command -v gs)" ] && [ ! -x "$(command -v pandoc)" ] [ ! -x "$(command -v ffmpeg)" ]
+	if [ ! -x "$(command -v gs)" ] && [ ! -x "$(command -v pandoc)" ] [ ! -x "$(command -v ffmpeg)" ] && [ ! -x "$(command -v say)"]
 	then
 		echo "Error: requirements not met"
 		echo "More info: https://github.com/dandrewbarlow/readpdf"
 		exit 1
 	fi
-		
 }
 
+# in case I (or you) fuck up
 usage() {
 	echo "readpdf- use OSX's native text-to-speech programs to read pdf text"
 	echo "USAGE: ./readpdf.sh -i [input] -v [voice] [-och]"
@@ -47,119 +48,136 @@ usage() {
     echo "i (argument) input - specify input file"
     echo "o (flag) output - if provided, outputs to input.aiff"
     echo "c (flag) convert - convert to mp3 using ffmpeg"
-    echo "v (argument) voice - specify voice to use. Defaults to Tom. No real error checking here, just passing it on to say so check what's available."
+    echo "v (argument) voice - specify voice to use. Defaults to Tom."
 	echo "for more information, visit https://github.com/dandrewbarlow/readpdf"
 
 }
 
-# check requirements
-requirements
-
-# parse options
-# options: -o -h -i -c
-# o : output- flag, not arg; create file with base filename, and write audio to it
-# h : help- display usage info
+# options: -i [input] -v [voice] -o -c -h 
 # i : input- argument, specify input file
 # v : voice- specify voice
+# o : output- flag, not arg; create file with base filename, and write audio to it
 # c : convert- flag, if present, convert to mp3
+# h : help- display usage info
+init() {
+	# check requirements before running
+	requirements
 
-while getopts "ohci:v:" opt; do
-	case $opt in
-		h)
-			usage
-			exit 1
-			;;
-		i)
-			if [ -e "$OPTARG" ]
-			then
-				input_file="$OPTARG"
-			else
-				echo "Error: file \"${OPTARG}\" does not exist"
+	# parse options and arguments given using getopts
+	while getopts "ohci:v:" opt; do
+		case $opt in
+			h)
+				usage
 				exit 1
-			fi
-			;;
-		o)
-			output_file=true
-			;;
-		c)
-			convert=true
-			if [ "$output_file" != true ]
-			then
-				echo "Can\'t convert without output"
+				;;
+			i)
+				if [ -e "$OPTARG" ]
+				then
+					input_file="$OPTARG"
+				else
+					echo "Error: file \"${OPTARG}\" does not exist"
+					exit 1
+				fi
+				;;
+			o)
+				output_file=true
+				;;
+			c)
+				convert=true
+				if [ "$output_file" != true ]
+				then
+					echo "Can\'t convert without output"
+					exit 1
+				fi
+				;;
+			v)
+				# validate voice choice
+				if [ ! -z "$(say -v '?' | grep -i ${OPTARG})" ]
+				then
+					voice="$OPTARG"
+				else
+					echo "Error: voice not found, defaulting to 'Tom'"
+				fi
+				;;
+			*)
+				usage
 				exit 1
-			fi
-			;;
-		v)
-			voice="$OPTARG"
-			;;
-		*)
-			usage
-			exit 1
-			;;
-	esac
-done
+				;;
+		esac
+	done
+}
 
-# check if the input file is specified and extant
-if [ "$input_file" != "" ] && [ -e "$input_file" ]
-then
-	# extract the name of the file and extension
-	name="$(echo "$input_file" | cut -f 1 -d '.')"
-	extension="$(echo "$input_file" | awk -F. '{print $NF}')"
+# execute the meaty instructions
+main() {
+	# initialize
+	init
 
-
-	# create temporary files if they are needed inside temp directory
-	temp_dir="$(mktemp -d)"
-	temp_text="${temp_dir}/text.txt)"
-	touch "$temp_text"
-
-	if [ "$convert" = true ]
-	then 
-		temp_audio="$(touch ${temp_dir}/audio.aiff)"
-	fi
-
-	# convert to txt
-	echo "> converting to txt"
-	if [ "$extension" == "pdf" ]
+	# check if the input file is specified and extant
+	if [ "$input_file" != "" ] && [ -e "$input_file" ]
 	then
-		gs -q -dNOPROMPT -dBATCH -dNOPAUSE -sDEVICE=txtwrite -sOutputFile="$temp_text" "$input_file"
-	elif [ "$extension" == "epub" ]
-	then
-		pandoc "$input_file" -f epub -t plain -o "$temp_text"
-	else
-		echo "Unsupported filetype, attempting implicit pandoc conversion"
-		
-		# if implicit conversion succeeds, this command will return an empty string
-		# if if does not return a zero type (-z), then exit the script
-		if [ ! -z "$(pandoc ${input_file} -t plain -o ${temp_text})" ]
-		then
-			exit 1
+		# extract the name of the file and extension
+		name="$(echo "$input_file" | cut -f 1 -d '.')"
+		extension="$(echo "$input_file" | awk -F. '{print $NF}')"
+
+
+		# create temporary files if they are needed inside temp directory
+		temp_dir="$(mktemp -d)"
+		temp_text="${temp_dir}/text.txt)"
+		touch "$temp_text"
+
+		if [ "$convert" = true ]
+		then 
+			temp_audio="$(touch ${temp_dir}/audio.aiff)"
 		fi
-	fi
 
-	# if output is true create one, else do it in place
-	# having trouble using pipes, so txt middle man might be necessary evil
-	if [ "$output_file" = true ]
-	then
-		echo "> generating audio"
-		if [ "$convert" = true ] 
+		# convert to txt
+		echo "> converting to txt"
+		if [ "$extension" == "pdf" ]
 		then
-			say -v 'Tom' -f "$temp_text" -o "$temp_audio"
-			# get the bitrate of output file and use that for equivalent conversion
-			bit="$(ffmpeg -i "${temp_audio}" 2>&1 | grep Audio | awk -F", " '{print $5}' | cut -d' ' -f1)"
-			echo "> converting audio"
-			ffmpeg -hide_banner -loglevel fatal -i "$temp_audio" -f mp3 -acodec libmp3lame -ab "$bit"k "${name}.mp3"
+			gs -q -dNOPROMPT -dBATCH -dNOPAUSE -sDEVICE=txtwrite -sOutputFile="$temp_text" "$input_file"
+		elif [ "$extension" == "epub" ]
+		then
+			pandoc "$input_file" -f epub -t plain -o "$temp_text"
 		else
-			say -v 'Tom' -f "$temp_text" -o "${name}.aiff"
+			echo "Unsupported filetype, attempting implicit pandoc conversion"
+
+			# if implicit conversion succeeds, this command will return an empty string
+			# if if does not return a zero type (-z), then exit the script
+			if [ ! -z "$(pandoc ${input_file} -t plain -o ${temp_text})" ]
+			then
+				exit 1
+			fi
 		fi
 
+		# if output is true create one, else do it in place
+		# having trouble using pipes, so txt middle man might be necessary evil
+		if [ "$output_file" = true ]
+		then
+			echo "> generating audio"
+			if [ "$convert" = true ] 
+			then
+				say -v 'Tom' -f "$temp_text" -o "$temp_audio"
+				# get the bitrate of output file and use that for equivalent conversion
+				bit="$(ffmpeg -i "${temp_audio}" 2>&1 | grep Audio | awk -F", " '{print $5}' | cut -d' ' -f1)"
+				echo "> converting audio"
+				ffmpeg -hide_banner -loglevel fatal -i "$temp_audio" -f mp3 -acodec libmp3lame -ab "$bit"k "./${name}.mp3"
+			else
+				say -v 'Tom' -f "$temp_text" -o "./${name}.aiff"
+			fi
+
+
+		else
+			echo "> starting narration"
+			say -v 'Tom' -f "$temp_text"
+		fi
 
 	else
-		echo "> starting narration"
-		say -v 'Tom' -f "$temp_text"
+		echo "Error: no input file specified"
+		echo "" 
+		usage
 	fi
+}
 
-else
-	echo "Error: no input file specified"
-	echo "" 
-	usage
-fi
+
+# the ol one liner
+main
